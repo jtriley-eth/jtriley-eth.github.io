f9e36a39fe7b07561fafc301df916b65bd9a5fe3
Joshua Trujillo <Jtriley15@gmail.com>
2023-07-27 15:46:48 -0700

    add app

---
+++ _app/immutable/chunks/1-2c67c4a8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-ba81f319.js";export{t as component};
---
+++ _app/immutable/chunks/2-4a9cc703.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-c18a9ed5.js";export{t as component};
