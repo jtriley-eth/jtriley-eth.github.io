8beb50f90c420149c56a52172f9183c7a9d3e5a6
Joshua Trujillo <Jtriley15@gmail.com>
2023-07-27 16:24:59 -0700

    try another hack

--- _app/immutable/chunks/2-473895ee.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-975b8af4.js";export{t as component};
---
+++ app/immutable/chunks/2-f3db701b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-b7b5b5f0.js";export{t as component};
