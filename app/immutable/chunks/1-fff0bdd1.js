ff13762b10cdeaed0ee8993432cd044a017a65e5
jtriley.eth <Jtriley15@gmail.com>
2023-11-29 13:58:02 -0600

    afuera!

--- app/immutable/chunks/1-fff0bdd1.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-be7eda63.js";export{t as component};
--- app/immutable/chunks/2-6cb9d6bb.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-6090c37f.js";export{t as component};
